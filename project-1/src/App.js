b97de36616f5eedc48b537835b00b3b599c5f7a2
Saichandan179 <masipeddisaichandan@gmail.com>
2022-02-05 16:37:11 -0500

    Add data

---
+++ project-1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BasicGrid from './DashBoard';
+
+function App() {
+  return (
+    <div className="App">
+     <BasicGrid/>
+    </div>
+  );
+}
+
+//change
+export default App;
